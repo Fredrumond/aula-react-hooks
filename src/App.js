87c9cf99525fdabd71d2638cba5763076e6a9004
Frederico Drumond <fredrumond@gmail.com>
2019-08-28 21:34:47 -0300

    aprendendo useEffect, useReducer e useMemo

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
-import React, { useState } from 'react';
+import React, {useRef, useState, useMemo } from 'react';
 import './App.css';
 import formSubmit from './formSubmit';
+import Contador from './Contador';
+import useTitleInput from './hooks/useTitleInput';
 
-const App = ()=> {
+const App = () => {
 
-  //const [valor,setValor] = useState('');
-  const [email,setEmail] = useState('teste@teste.com');
-  
+  const [email, setEmail] = useTitleInput('teste@hotmail.com');
+  const [nome, setNome] = useState('');
+
+  const countString = (string) => {
+    return string.length;
+  };
+
+  //const countedName = countString(nome);
+
+  const countedName = useMemo(() => countString(nome),[nome]);
+
+  const area = useRef();
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
-
-    formSubmit(email,setEmail);
-  }
-
+    formSubmit(email, setEmail);
+  };
   return (
-    <div>
-      <h1>Ola</h1>
+    <div ref={area}>
 
-      <form onSubmit={handleFormSubmit}> 
-        <label>Qual é seu E-mail?</label>
-        <input id="email" type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
-        
-        <input className="submit" type="email" type="submit"/>
+      <button onClick={() => {
+        area.current.classList.remove("maior");
+        area.current.classList.add("menor");
+      }}>Fonte -</button>
+      <button onClick={() => {
+        area.current.classList.remove("menor");
+        area.current.classList.add("maior");
+      }}>Fonte +</button>
+
+      <h1>Bem Vindo(a)</h1>
+
+      <form onSubmit={handleFormSubmit}>
+      <label>Qual Seu Nome?</label>
+        <input
+          id="nome"
+          type="nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value) }
+        />
+        <p>{countedName} letras</p>
+        <label>Qual Seu E-mail?</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value) }
+        />
+
+        <input className="submit" type="submit" value="Enviar" />
       </form>
+
+      <hr />
+
+      <Contador />
       
-    </div>    
+
+    </div>
   );
-}
+};
 
 export default App;
